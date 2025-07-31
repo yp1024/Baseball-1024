eda19ba77a5377030e0ce50e7ad1418504b373fc
yunspark <yun.park.kor@gmail.com>
2025-07-31 14:28:38 +0900

    [refactor] page 49

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 class Game:
-    def guess(self, guessNumber):
-        if guessNumber is None:
-            raise TypeError()
+    def guess(self, guess_number):
+        self._assert_illegal_value(guess_number)
 
-        if len(guessNumber) != 3:
-            raise TypeError()
+    def _assert_illegal_value(self, guess_number):
+        if guess_number is None:
+            raise TypeError('입력이 None 입니다')
 
-        for number in guessNumber:
-            if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError()
+        if len(guess_number) != 3:
+            raise TypeError('입력은 3 자리 문자열이어야 합니다')
 
-        if guessNumber[0] == guessNumber[1] or \
-                guessNumber[0] == guessNumber[2] or \
-                guessNumber[1] == guessNumber[2]:
-            raise TypeError()
+        if not guess_number.isdigit():
+            raise TypeError('모든 문자는 숫자로 구성되어야 합니다')
+
+        if self._is_duplicated_number(guess_number):
+            raise TypeError('중복된 숫자가 존재합니다')
+
+    def _is_duplicated_number(self, guess_number):
+        return len(set(guess_number)) != 3
