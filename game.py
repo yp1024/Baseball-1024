4475cd7ce9ab0eb2ef3aa67cf0c39cbe352c0562
yunspark <yun.park.kor@gmail.com>
2025-07-31 14:51:22 +0900

    [refactor] page 58

--- game.py
+++ game.py
@@ -1,9 +1,22 @@
 from game_result import GameResult
 
 class Game:
-    def guess(self, guess_number) -> GameResult:
+    def __init__(self):
+        self._question = ''
+
+    @property
+    def question(self):
+        raise AttributeError('읽을 수 없는 속성')
+
+    @question.setter
+    def question(self, value):
+        self._question = value
+
+    def guess(self, guess_number) -> GameResult | None:
         self._assert_illegal_value(guess_number)
-        return GameResult(True, 3, 0)
+        if guess_number == self._question:
+            return GameResult(True, 3, 0)
+        return None
 
     def _assert_illegal_value(self, guess_number):
         if guess_number is None:
